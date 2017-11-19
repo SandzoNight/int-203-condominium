16f2f90dc7323275712a00fe7f1e707eeef716ea
SandzoNight <flukemdc@gmail.com>
2017-11-20 04:15:50 +0700

    include bootstrap css

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import Routes from './routes';
 import createBrowserHistory from 'history/createBrowserHistory'
-
+import './css/bootstrap.css'
 const customHistory = createBrowserHistory()
 
-ReactDOM.render(<Routes history={customHistory} />, document.getElementById('root'));
+ReactDOM.render(<Routes history={customHistory}/>, document.getElementById('root'));
 registerServiceWorker();
